ce14b0f890901109ba22d49667b67ed20a06d884
R. Miles McCain <github@sendmiles.email>
2022-03-16 16:32:05 -0700

    Basic shell

--- platform/assets/tailwind.config.js
+++ platform/assets/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       colors: {
         neutral: colors.slate,
         positive: colors.green,
-        urge: colors.violet,
+        urge: colors.indigo,
         warning: colors.yellow,
         info: colors.blue,
         critical: colors.red,
