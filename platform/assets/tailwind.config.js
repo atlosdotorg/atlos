e70704f064c0f5f271a692de1db00b178a66327e
R. Miles McCain <github@sendmiles.email>
2022-03-15 17:15:57 -0700

    Add a17t

--- platform/assets/tailwind.config.js
+++ platform/assets/tailwind.config.js
@@ -1,11 +1,25 @@
+let colors = require("tailwindcss/colors")
+
 module.exports = {
   content: [
     './js/**/*.js',
     '../lib/*_web/**/*.*ex'
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        neutral: colors.slate,
+        positive: colors.green,
+        urge: colors.violet,
+        warning: colors.yellow,
+        info: colors.blue,
+        critical: colors.red,
+      },
+    },
+  },
   variants: {},
   plugins: [
     require('@tailwindcss/forms'),
+    require('a17t'),
   ]
 };
