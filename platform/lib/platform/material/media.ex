78e81ddf56411c609ac6d99804c010a071f1b0f5
R. Miles McCain <github@sendmiles.email>
2022-03-29 21:18:29 -0700

    Attribute refactoring

--- platform/lib/platform/material/media.ex
+++ platform/lib/platform/material/media.ex
@@ -24,9 +24,10 @@ defmodule Platform.Material.Media do
     media
     |> cast(attrs, [:description, :attr_sensitive])
     |> validate_required([:description])
-    |> validate_length(:description, min: 8, max: 240)
-    # This is a special attribute, since we define it at creation time. Eventually, it'd be nice to unify this logic with the attribute-specific editing logic.
+
+    # These are special attributes, since we define it at creation time. Eventually, it'd be nice to unify this logic with the attribute-specific editing logic.
     |> Attribute.validate_attribute(Attribute.get_attribute(:sensitive))
+    |> Attribute.validate_attribute(Attribute.get_attribute(:description))
   end
 end
 
@@ -35,7 +36,7 @@ defmodule Platform.Material.Media.Attribute do
   import Ecto.Changeset
   alias __MODULE__
 
-  defstruct [:schema_field, :type, :options, :label]
+  defstruct [:schema_field, :type, :label, :options, :max_length, :min_length]
 
   defp attributes() do
     %{
@@ -44,8 +45,30 @@ defmodule Platform.Material.Media.Attribute do
         type: :multi_select,
         options: ["Threatens Civilian Safety", "Graphic Violence"],
         label: "Sensitivity"
+      },
+      description: %Attribute{
+        schema_field: :description,
+        type: :text,
+        max_length: 255,
+        min_length: 8,
+        label: "Short Description"
       }
     }
+  end
+
+  @doc """
+  Get the names of the attributes that are available for the given media.
+  """
+  def set_for_media(media) do
+    Enum.filter(list_attributes(), fn attr_name ->
+      attr = get_attribute(attr_name)
+      Map.get(media, attr.schema_field) != nil
+    end)
+  end
+
+  def unset_for_media(media) do
+    set = set_for_media(media)
+    Enum.filter(list_attributes(), &(!Enum.member?(set, &1)))
   end
 
   def get_attribute(name) do
@@ -66,6 +89,8 @@ defmodule Platform.Material.Media.Attribute do
     case attribute.type do
       :multi_select ->
         changeset |> validate_subset(attribute.schema_field, attribute.options)
+      :text ->
+        changeset |> validate_length(attribute.schema_field, min: attribute.min_length, max: attribute.max_length)
     end
   end
 end
--- platform/lib/platform_web/live/media_live/show.ex
+++ platform/lib/platform_web/live/media_live/show.ex
@@ -1,9 +1,36 @@
 defmodule PlatformWeb.MediaLive.Show do
   use PlatformWeb, :live_view
   alias Platform.Material
+  alias Material.Media.Attribute
 
   def mount(%{"slug" => slug} = _params, _session, socket) do
     {:ok, socket |> assign(:slug, slug) |> assign_media()}
+  end
+
+  defp attr_entry(assigns) do
+    attr = Attribute.get_attribute(assigns.name)
+    ~H"""
+    <div class="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+        <dt class="text-sm font-medium text-gray-500 mt-1"><%= attr.label %></dt>
+        <dd class="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <span class="flex-grow">
+                <%= case attr.type do %>
+                <% :text -> %>
+                <div class="mr-1 my-1 inline-block">
+                  <%= Map.get(@media, attr.schema_field) %>
+                </div>
+                <% :multi_select -> %>
+                  <%= for item <- Map.get(@media, attr.schema_field) do %>
+                      <span class="chip ~neutral mr-1 mb-1"><%= item %></span>
+                  <% end %>
+                <% end %>
+            </span>
+            <span class="ml-4 flex-shrink-0">
+                <button type="button" class="text-button mt-1">Update</button>
+            </span>
+        </dd>
+    </div>
+    """
   end
 
   defp assign_media(socket) do
