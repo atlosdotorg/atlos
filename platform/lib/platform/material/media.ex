4440f26ff4e9ff7e4a4ac45bd54ae92c99d60a2b
R. Miles McCain <github@sendmiles.email>
2022-03-30 21:10:04 -0700

    Add different panes

--- platform/lib/platform/material/media.ex
+++ platform/lib/platform/material/media.ex
@@ -39,32 +39,58 @@ defmodule Platform.Material.Media.Attribute do
   import Ecto.Changeset
   alias __MODULE__
 
-  defstruct [:schema_field, :type, :label, :options, :max_length, :min_length, :pane]
+  defstruct [
+    :schema_field,
+    :type,
+    :label,
+    :options,
+    :max_length,
+    :min_length,
+    :pane,
+    :required,
+    :custom_validation
+  ]
 
   defp attributes() do
     %{
       sensitive: %Attribute{
         schema_field: :attr_sensitive,
         type: :multi_select,
-        options: ["Not Sensitive", "Threatens Civilian Safety", "Graphic Violence", "Deleted by Original Poster"],
+        options: [
+          "Not Sensitive",
+          "Threatens Civilian Safety",
+          "Graphic Violence",
+          "Deleted by Source",
+          "Deceptive or Misleading"
+        ],
         label: "Sensitivity",
         min_length: 1,
-        pane: :metadata
+        pane: :metadata,
+        required: true,
+        custom_validation: fn :attr_sensitive, vals ->
+          if Enum.member?(vals, "Not Sensitive") && length(vals) > 1 do
+            [attr_sensitive: "If the media is 'Not Sensitive,' no other options may be selected"]
+          else
+            []
+          end
+        end
       },
       description: %Attribute{
         schema_field: :description,
         type: :text,
         max_length: 240,
         min_length: 8,
-        label: "Short Description",
-        pane: :metadata
+        label: "Description",
+        pane: :metadata,
+        required: true
       },
       time_of_day: %Attribute{
         schema_field: :attr_time_of_day,
         type: :select,
         options: ["Night", "Day"],
         label: "Time of Day",
-        pane: :attributes
+        pane: :attributes,
+        required: false
       }
     }
   end
@@ -72,16 +98,19 @@ defmodule Platform.Material.Media.Attribute do
   @doc """
   Get the names of the attributes that are available for the given media.
   """
-  def set_for_media(media) do
+  def set_for_media(media, pane \\ nil) do
     Enum.filter(list_attributes(), fn attr_name ->
       attr = get_attribute(attr_name)
-      Map.get(media, attr.schema_field) != nil
+      Map.get(media, attr.schema_field) != nil && (pane == nil || attr.pane == pane)
     end)
   end
 
-  def unset_for_media(media) do
+  def unset_for_media(media, pane \\ nil) do
     set = set_for_media(media)
-    Enum.filter(list_attributes(), &(!Enum.member?(set, &1)))
+
+    set
+    |> Enum.filter(&(!Enum.member?(set, &1)))
+    |> Enum.filter(&(pane == nil || get_attribute(&1).pane == pane))
   end
 
   def get_attribute(name) do
@@ -99,25 +128,34 @@ defmodule Platform.Material.Media.Attribute do
   end
 
   def validate_attribute(changeset, %Attribute{} = attribute) do
-    case attribute.type do
-      :multi_select ->
-        changeset
-        |> validate_subset(attribute.schema_field, attribute.options)
-        |> validate_required(attribute.schema_field)
-        |> validate_length(attribute.schema_field,
-          min: attribute.min_length,
-          max: attribute.max_length
-        )
-      :select ->
+    validations =
+      case attribute.type do
+        :multi_select ->
+          changeset
+          |> validate_subset(attribute.schema_field, attribute.options)
+          |> validate_required(attribute.schema_field)
+          |> validate_length(attribute.schema_field,
+            min: attribute.min_length,
+            max: attribute.max_length
+          )
+
+        :select ->
           changeset
           |> validate_inclusion(attribute.schema_field, attribute.options)
           |> validate_required(attribute.schema_field)
-      :text ->
-        changeset
-        |> validate_length(attribute.schema_field,
-          min: attribute.min_length,
-          max: attribute.max_length
-        )
+
+        :text ->
+          changeset
+          |> validate_length(attribute.schema_field,
+            min: attribute.min_length,
+            max: attribute.max_length
+          )
+      end
+
+    if attribute.custom_validation != nil do
+      validations |> validate_change(attribute.schema_field, attribute.custom_validation)
+    else
+      validations
     end
   end
 end
