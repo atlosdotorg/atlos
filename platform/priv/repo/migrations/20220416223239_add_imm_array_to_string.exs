48b8cff249d292578adaf80d0cef63c933f0029e
R. Miles McCain <github@sendmiles.email>
2022-04-16 17:13:54 -0700

    Fix migrations

--- platform/priv/repo/migrations/20220416223239_add_imm_array_to_string.exs
+++ platform/priv/repo/migrations/20220416223239_add_imm_array_to_string.exs
@@ -4,7 +4,7 @@ defmodule Platform.Repo.Migrations.AddImmArrayToString do
   def change do
     execute """
               CREATE OR REPLACE FUNCTION imm_array_to_string(character varying[], text, text)
-              RETURNS text LANGUAGE sql IMMUTABLE AS $$select array_to_string(coalesce($1, {}), $2, $3)$$
+              RETURNS text LANGUAGE sql IMMUTABLE AS $$select array_to_string(coalesce($1, '{}'), $2, $3)$$
             """,
             ""
   end
