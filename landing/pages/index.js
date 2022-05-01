85e5d90ea1270fbde2d5117a20038610a101141c
R. Miles McCain <github@sendmiles.email>
2022-04-30 17:14:13 -0700

    Fix landing page

--- landing/pages/index.js
+++ landing/pages/index.js
@@ -44,20 +44,20 @@ export default function Home() {
         </div>
         <section className='z-10 fixed flex flex-col space-y-6 top-0 mx-auto p-6 md:m-12 lg:m-36 md:max-w-lg'>
           <p className='text-5xl'>
-            <span class="bg-white rounded-sm font-mono text-slate-900 py-1 px-2 font-bold">ATLOS</span>
+            <span className="bg-white rounded-sm font-mono text-slate-900 py-1 px-2 font-bold">ATLOS</span>
           </p>
           <p className="text-2xl font-semibold md:text-3xl text-white pt-2">
             The open source platform for visual investigations
           </p>
           <div className='text-white text-md flex flex-col space-y-3'>
             <p>
-              Geolocate media, build on other researchers' findings, and double-check everyone's work. Search our catalog to piece together the bigger picture.
+              Geolocate media, build on other researchers&apos; findings, and double-check everyone&apos;s work. Search our catalog to piece together the bigger picture.
             </p>
             <p>
               We help OSINT researchers and organizations collaborate so they can focus on content, not coordination. <a href="https://atlos.notion.site/Platform-Overview-46d4723f22ef420fb5ad0e07feba8d79" className='link'>Preview our platform.</a>
             </p>
             <p>
-              OSINT research can be taxing; Atlos puts researchers' safety and mental health first. <a href="https://atlos.notion.site/Our-Approach-to-Safety-3c1b9842128a4149b3c60c32773c8e5a" className='link'>Learn about our approach to safety.</a>
+              OSINT research can be taxing; Atlos puts researchers&apos; safety and mental health first. <a href="https://atlos.notion.site/Our-Approach-to-Safety-3c1b9842128a4149b3c60c32773c8e5a" className='link'>Learn about our approach to safety.</a>
             </p>
             <p>
               If you or your organization is interested in working with us, please <a href="mailto:contact@atlos.org" className='link'>reach out</a> or join our mailing list. <a href="https://mailchi.mp/a1de52cd4614/atlos" className='link'>Sign up to receive updates.</a>
