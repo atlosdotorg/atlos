c097338033c832621f52538e243ce7b27d5656fd
R. Miles McCain <github@sendmiles.email>
2022-05-03 15:49:54 -0700

    Update tests

--- platform/test/platform_web/controllers/user_session_controller_test.exs
+++ platform/test/platform_web/controllers/user_session_controller_test.exs
@@ -31,10 +31,10 @@ defmodule PlatformWeb.UserSessionControllerTest do
       assert get_session(conn, :user_token)
       assert redirected_to(conn) == "/"
 
-      # Now do a logged in request and verify they are redirected to /media
+      # Now do a logged in request and verify they are redirected to /map
       conn = get(conn, "/")
       response = html_response(conn, 302)
-      assert response =~ "/media"
+      assert response =~ "/map"
     end
 
     test "logs the user in with remember me", %{conn: conn, user: user} do
