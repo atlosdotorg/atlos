b65180dfd5736c104324bbaed16f7f26970bb517
R. Miles McCain <github@sendmiles.email>
2022-05-02 17:21:43 -0700

    Reduce bio debounce

--- platform/lib/platform_web/live/settings_live/profile_component.ex
+++ platform/lib/platform_web/live/settings_live/profile_component.ex
@@ -134,7 +134,7 @@ defmodule PlatformWeb.SettingsLive.ProfileComponent do
           <div>
             <%= label(f, :bio) %>
             <div class="mt-1">
-              <%= textarea(f, :bio, rows: 4, phx_debounce: "blur") %>
+              <%= textarea(f, :bio, rows: 4, phx_debounce: 100) %>
             </div>
             <%= error_tag(f, :bio) %>
           </div>
