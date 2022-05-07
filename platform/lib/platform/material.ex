0b3dcc572410e02eb10c97dce31120d29bd5f9cb
R. Miles McCain <github@sendmiles.email>
2022-05-07 15:30:12 -0700

    Add debug message for font path

--- platform/lib/platform/material.ex
+++ platform/lib/platform/material.ex
@@ -431,6 +431,8 @@ defmodule Platform.Material do
         Path.join(Application.app_dir(:platform), "priv/static/fonts/iosevka-bold.ttc")
       )
 
+    IO.puts("Loading font from #{font_path}; file exists? #{File.exists?(font_path)}")
+
     process_command =
       FFmpex.new_command()
       |> FFmpex.add_input_file(path)
