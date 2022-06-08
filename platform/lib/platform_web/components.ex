5b44af387315b3832408f3f1ba41ca9ac732dfd9
R. Miles McCain <github@sendmiles.email>
2022-06-08 15:43:44 -0400

    Add warning on source click

--- platform/lib/platform_web/components.ex
+++ platform/lib/platform_web/components.ex
@@ -1029,6 +1029,7 @@ defmodule PlatformWeb.Components do
                   href={version.source_url}
                   rel="nofollow"
                   class="button mt-1 original py-1 px-2 text-xs"
+                  data-confirm="This link will open an external site in a new tab. Are you sure?"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -1039,7 +1040,7 @@ defmodule PlatformWeb.Components do
                     <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
                     <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
                   </svg>
-                  View Directly
+                  View Source
                 </a>
               </div>
             <% end %>
@@ -1053,6 +1054,7 @@ defmodule PlatformWeb.Components do
             href={version.source_url}
             rel="nofollow"
             class="button original py-1 px-2"
+            data-confirm="This link will open an external site in a new tab. Are you sure?"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
