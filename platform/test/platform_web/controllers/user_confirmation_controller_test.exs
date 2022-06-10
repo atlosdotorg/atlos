b850ac04ad6336cd44fb43b5b6725e463dec21ac
R. Miles McCain <github@sendmiles.email>
2022-06-10 17:56:02 -0400

    Fix confirmation tests

--- platform/test/platform_web/controllers/user_confirmation_controller_test.exs
+++ platform/test/platform_web/controllers/user_confirmation_controller_test.exs
@@ -13,7 +13,7 @@ defmodule PlatformWeb.UserConfirmationControllerTest do
     test "renders the resend confirmation page", %{conn: conn} do
       conn = get(conn, Routes.user_confirmation_path(conn, :new))
       response = html_response(conn, 200)
-      assert response =~ "<h1>Resend confirmation instructions</h1>"
+      assert response =~ "Send instructions"
     end
   end
 
@@ -59,7 +59,7 @@ defmodule PlatformWeb.UserConfirmationControllerTest do
     test "renders the confirmation page", %{conn: conn} do
       conn = get(conn, Routes.user_confirmation_path(conn, :edit, "some-token"))
       response = html_response(conn, 200)
-      assert response =~ "Confirm account</h1>"
+      assert response =~ "Confirm my account"
 
       form_action = Routes.user_confirmation_path(conn, :update, "some-token")
       assert response =~ "action=\"#{form_action}\""
