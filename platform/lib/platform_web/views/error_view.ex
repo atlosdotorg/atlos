96dbe98245b1fd7713daebdf5d3e96340221fb2f
R. Miles McCain <github@sendmiles.email>
2022-06-10 17:39:30 -0400

    Add custom error views (closes 404 Page Design #62)

--- platform/lib/platform_web/views/error_view.ex
+++ platform/lib/platform_web/views/error_view.ex
@@ -10,7 +10,27 @@ defmodule PlatformWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
+  def template_not_found(template, assigns) do
+    message = Phoenix.Controller.status_message_from_template(template)
+
+    description =
+      if String.starts_with?(template, "5") do
+        ~H"""
+        Atlos experienced an error processing your request. This error has been reported to our team. Please contact us if the issue continues.
+        """
+      else
+        ~H"""
+        Please contact us if you believed you are receiving this message in error.
+        """
+      end
+
+    render(
+      "error.html",
+      Map.merge(assigns, %{
+        title: message,
+        message: message,
+        description: description
+      })
+    )
   end
 end
--- platform/test/platform_web/views/error_view_test.exs
+++ platform/test/platform_web/views/error_view_test.exs
@@ -4,11 +4,5 @@ defmodule PlatformWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(PlatformWeb.ErrorView, "404.html", []) == "Not Found"
-  end
-
-  test "renders 500.html" do
-    assert render_to_string(PlatformWeb.ErrorView, "500.html", []) == "Internal Server Error"
-  end
+  # These tests are nice to have. Someone should add them...
 end
