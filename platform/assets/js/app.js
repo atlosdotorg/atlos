141525eb5b5ea44c0ff79c92d495215d916795d5
R. Miles McCain <github@sendmiles.email>
2022-07-18 22:08:39 -0700

    Improve map performance

--- platform/assets/js/app.js
+++ platform/assets/js/app.js
@@ -39,6 +39,19 @@ let liveSocket = new LiveSocket("/live", Socket, {
     params: { _csrf_token: csrfToken }
 })
 
+/**
+ * https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro
+ * 
+ * @param {String} HTML representing a single element
+ * @return {Element}
+ */
+function htmlToElement(html) {
+    var template = document.createElement('template');
+    html = html.trim(); // Never return a text node of whitespace as the result
+    template.innerHTML = html;
+    return template.content.firstChild;
+}
+
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", info => topbar.show())
@@ -147,25 +160,84 @@ function initializeMaps() {
             map.resize();
         });
 
-        let data = JSON.parse(s.getAttribute("data"));
-        for (let incident of data) {
-            let popup = new mapboxgl.Popup({ offset: 30, closeButton: false }).setHTML(`
-                <div class="fixed w-[350px] h-[190px] flex rounded-lg shadow-lg items-center bg-white justify-around -z-50">
-                    <div class="font-medium text-lg text-md p-4">
-                        <span class="animate-pulse">Loading...</span>
-                    </div>
-                </div>
-                <iframe
-                    src='/incidents/${incident.slug}/card'
-                    width="350px"
-                    height="190px"
-                />
-            `).setMaxWidth("600px");
-            new mapboxgl.Marker({ color: "#60a5fa90" })
-                .setLngLat([incident.lon, incident.lat])
-                .setPopup(popup)
-                .addTo(map);
-        }
+        map.on("load", () => {
+            let data = JSON.parse(s.getAttribute("data"));
+
+            let geojson = {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": data.map(incident => {
+                        return {
+                            "type": "Feature",
+                            "properties": {
+                                "description": `
+                                <div class="fixed w-[350px] h-[190px] flex rounded-lg shadow-lg items-center bg-white justify-around -z-50">
+                                    <div class="font-medium text-lg text-md p-4">
+                                        <span class="animate-pulse">Loading...</span>
+                                    </div>
+                                </div>
+                                <iframe
+                                    src='/incidents/${incident.slug}/card'
+                                    width="350px"
+                                    height="190px"
+                                />
+                            `,
+                                "slug": incident.slug
+                            },
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': [incident.lon, incident.lat]
+                            }
+                        }
+                    })
+                }
+            };
+
+            map.addSource("incidents", geojson);
+
+            map.addLayer({
+                'id': 'incidents',
+                'type': 'circle',
+                'source': 'incidents',
+                'paint': {
+                    'circle-radius': 7,
+                    'circle-color': '#60a5fa',
+                    'circle-opacity': 0.6,
+                },
+            });
+
+            // When a click event occurs on a feature in the places layer, open a popup at the
+            // location of the feature, with description HTML from its properties.
+            map.on('click', 'incidents', (e) => {
+                // Copy coordinates array.
+                const coordinates = e.features[0].geometry.coordinates.slice();
+                const description = e.features[0].properties.description;
+
+                // Ensure that if the map is zoomed out such that multiple
+                // copies of the feature are visible, the popup appears
+                // over the copy being pointed to.
+                while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                }
+
+                new mapboxgl.Popup({ closeButton: false })
+                    .setLngLat(coordinates)
+                    .setHTML(description)
+                    .setMaxWidth("600px")
+                    .addTo(map);
+            });
+
+            // Change the cursor to a pointer when the mouse is over the places layer.
+            map.on('mouseenter', 'places', () => {
+                map.getCanvas().style.cursor = 'pointer';
+            });
+
+            // Change it back to a pointer when it leaves.
+            map.on('mouseleave', 'places', () => {
+                map.getCanvas().style.cursor = '';
+            });
+        });
     });
 }
 
