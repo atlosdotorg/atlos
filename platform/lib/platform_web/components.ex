c0a492ebd9331f3425805e79b84d912c1fa727eb
R. Miles McCain <github@sendmiles.email>
2022-08-20 15:08:06 -0700

    Improve modal close behavior

--- platform/lib/platform_web/components.ex
+++ platform/lib/platform_web/components.ex
@@ -33,7 +33,11 @@ defmodule PlatformWeb.Components do
       aria-modal="true"
     >
       <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true">
+        <div
+          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          aria-hidden="true"
+          phx-click="close_modal"
+        >
         </div>
         <!-- This element is to trick the browser into centering the modal contents. -->
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
@@ -42,7 +46,6 @@ defmodule PlatformWeb.Components do
 
         <div
           class="relative inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full sm:p-6"
-          phx-click-away="close_modal"
           phx-window-keydown="close_modal"
           phx-key="Escape"
           phx-target={@target}
