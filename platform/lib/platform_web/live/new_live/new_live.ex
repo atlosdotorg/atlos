208bb8cfa34e10020f94e9f3573b4dac95397ee9
R. Miles McCain <github@sendmiles.email>
2022-08-20 15:08:52 -0700

    Make incident creation a single step

--- platform/lib/platform_web/live/new_live/new_live.ex
+++ platform/lib/platform_web/live/new_live/new_live.ex
@@ -8,21 +8,16 @@ defmodule PlatformWeb.NewLive do
   end
 
   def handle_info({:media_created, media}, socket) do
-    {:noreply, socket |> assign(:media, media) |> assign(:stage, "Upload media")}
-  end
-
-  def handle_info({:version_created, _version}, socket) do
     {:noreply,
      socket
-     |> put_flash(:info, "Successfully added media.")
-     |> redirect(to: Routes.media_show_path(socket, :show, socket.assigns.media.slug))}
+     |> put_flash(:info, "Incident created successfully.")
+     |> redirect(to: "/incidents/#{media.slug}/upload")}
   end
 
   def render(assigns) do
     ~H"""
     <div class="space-y-8 max-w-xl md:mx-auto mx-4">
       <h1 class="page-header">New Incident</h1>
-      <.stepper options={["Basic info", "Upload media"]} active={@stage} />
 
       <%= if @stage == "Basic info" do %>
         <.card>
@@ -66,30 +61,6 @@ defmodule PlatformWeb.NewLive do
             module={PlatformWeb.NewLive.BasicInfoLive}
             id="basic-info"
             current_user={@current_user}
-          />
-        </.card>
-      <% end %>
-
-      <%= if @stage == "Upload media" do %>
-        <.card>
-          <:header>
-            <h3 class="sec-head">
-              <%= @media.description %>
-              <%= if @media.attr_sensitive do %>
-                <span class="text-neutral-500">
-                  (Sensitivity: <%= Enum.join(@media.attr_sensitive, ", ") %>)
-                </span>
-              <% end %>
-            </h3>
-            <p class="sec-subhead">
-              You can upload additional media later. This media will be assigned the Atlos identifier <%= @media.slug %>.
-            </p>
-          </:header>
-          <.live_component
-            module={PlatformWeb.MediaLive.CreateMediaVersion}
-            id="upload-version"
-            current_user={@current_user}
-            media={@media}
           />
         </.card>
       <% end %>
