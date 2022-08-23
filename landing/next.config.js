4dbe65fd3a13ba5d3b4f125870514c89dfc25cce
R. Miles McCain <github@sendmiles.email>
2022-08-22 19:16:13 -0700

    Add waitlist to landing page

--- landing/next.config.js
+++ landing/next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/waitlist',
+        destination: 'https://forms.gle/Hi8ChipVJfMgMrtHA',
+        permanent: false,
+      },
+    ]
+  },
 }
