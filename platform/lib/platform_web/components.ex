2910e7391e346a15c09d13c5d0f1f51d2726783c
R. Miles McCain <github@sendmiles.email>
2022-10-16 12:27:10 -0700

    Use dots in multibar, not gear (closes #328)

--- platform/lib/platform_web/components.ex
+++ platform/lib/platform_web/components.ex
@@ -1453,7 +1453,7 @@ defmodule PlatformWeb.Components do
               <%= error_tag(f, :sort) %>
             </div>
             <div
-              class="flex place-self-center w-full md:w-auto h-full px-2 text-sm md:py-[18px] py-4"
+              class="flex place-self-center w-full md:w-auto h-full px-2 text-sm md:py-[14px] py-4"
               x-data="{open: false}"
             >
               <div class="text-left z-10">
@@ -1461,7 +1461,7 @@ defmodule PlatformWeb.Components do
                   <button
                     x-on:click="open = !open"
                     type="button"
-                    class="rounded-full flex items-center align-center text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-urge-500"
+                    class="rounded-full flex items-center align-center text-gray-600 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-urge-500"
                     id="menu-button"
                     aria-expanded="true"
                     aria-haspopup="true"
@@ -1470,16 +1470,17 @@ defmodule PlatformWeb.Components do
                     <!-- Heroicon name: solid/dots-vertical -->
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
+                      viewBox="0 0 24 24"
                       fill="currentColor"
-                      class="w-4 h-4"
+                      class="w-6 h-6"
                     >
                       <path
                         fill-rule="evenodd"
-                        d="M7.84 1.804A1 1 0 018.82 1h2.36a1 1 0 01.98.804l.331 1.652a6.993 6.993 0 011.929 1.115l1.598-.54a1 1 0 011.186.447l1.18 2.044a1 1 0 01-.205 1.251l-1.267 1.113a7.047 7.047 0 010 2.228l1.267 1.113a1 1 0 01.206 1.25l-1.18 2.045a1 1 0 01-1.187.447l-1.598-.54a6.993 6.993 0 01-1.929 1.115l-.33 1.652a1 1 0 01-.98.804H8.82a1 1 0 01-.98-.804l-.331-1.652a6.993 6.993 0 01-1.929-1.115l-1.598.54a1 1 0 01-1.186-.447l-1.18-2.044a1 1 0 01.205-1.251l1.267-1.114a7.05 7.05 0 010-2.227L1.821 7.773a1 1 0 01-.206-1.25l1.18-2.045a1 1 0 011.187-.447l1.598.54A6.993 6.993 0 017.51 3.456l.33-1.652zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                        d="M10.5 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"
                         clip-rule="evenodd"
                       />
                     </svg>
+
                     <span class="md:hidden text-neutral-800 ml-1">Additional options</span>
                   </button>
                 </div>
