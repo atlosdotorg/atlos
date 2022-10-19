ff9150656f79857cfb1ca707fd36c62d1f7d6891
R. Miles McCain <github@sendmiles.email>
2022-10-19 13:45:53 -0700

    Draft landing page improvements

--- landing/pages/_app.js
+++ landing/pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import "@fontsource/iosevka/800.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
