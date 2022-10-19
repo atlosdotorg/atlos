b99c938522a7c1aa9e6887879c0045e929cb820b
R. Miles McCain <github@sendmiles.email>
2022-10-19 13:47:21 -0700

    Use locally hosted Inter

--- landing/pages/_app.js
+++ landing/pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
 import "@fontsource/iosevka/800.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/700.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
