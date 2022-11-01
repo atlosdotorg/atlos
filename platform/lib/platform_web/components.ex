372cbf09108323234e436ee3b1f77aa8dfe9a785
R. Miles McCain <github@sendmiles.email>
2022-11-01 22:36:26 +0000

    Improve media cards

--- platform/lib/platform_web/components.ex
+++ platform/lib/platform_web/components.ex
@@ -1963,19 +1963,6 @@ defmodule PlatformWeb.Components do
             <%= if @sensitive do %>
               <%= for item <- @media.attr_sensitive || [] do %>
                 <span class={"self-start badge " <> Attribute.attr_color(:sensitive, @media.attr_sensitive)}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-3 w-3 mr-px"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-
                   <%= item %>
                 </span>
               <% end %>
@@ -1984,86 +1971,39 @@ defmodule PlatformWeb.Components do
             <%= for item <- @media.attr_restrictions || [] do %>
               <!-- TODO: make this use Attribute.attr_color/2 -->
               <span class="self-start badge ~warning">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-3 w-3 mr-px"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
-                  />
-                </svg>
                 <%= item %>
               </span>
             <% end %>
 
             <%= if @media.attr_geolocation do %>
               <span class="self-start badge ~neutral">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-3 w-3 mr-px"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
                 Geolocated
               </span>
             <% end %>
 
             <%= if @media.attr_date do %>
               <span class="self-start badge ~neutral">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-3 w-3 mr-px"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <%= @media.attr_date %>
+                <%= @media.attr_date |> Calendar.strftime("%d %B %Y") %>
               </span>
             <% end %>
 
+            <%= if @media.attr_type do %>
             <span class="self-start badge ~neutral">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-3 w-3 mr-px"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
-                <path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
-                <path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
-              </svg>
-              <%= length(Attribute.set_for_media(@media)) %> Attrs
+              <%= hd(@media.attr_type) %>
             </span>
+            <% end %>
 
+            <%= if @media.attr_equipment do %>
             <span class="self-start badge ~neutral">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-3 w-3 mr-px"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z" />
-                <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z" />
-              </svg>
-              <%= length(@media.updates) %> Updates
+              <%= hd(@media.attr_equipment) %>
             </span>
+            <% end %>
+
+            <%= if @media.attr_impact do %>
+            <span class="self-start badge ~neutral">
+              <%= hd(@media.attr_impact) %>
+            </span>
+            <% end %>
           </section>
           <section class="mb-2 h-4" />
           <section class="bottom-0 mb-2 pr-4 w-full absolute flex gap-2 justify-between items-center">
@@ -2071,7 +2011,7 @@ defmodule PlatformWeb.Components do
             <p class="text-xs text-gray-500 flex items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 mr-px text-gray-400"
+                class="h-3 w-3 mr-[3px] text-gray-400"
                 viewBox="0 0 20 20"
                 fill="currentColor"
               >
