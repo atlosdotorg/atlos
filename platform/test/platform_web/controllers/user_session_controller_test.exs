b20ae6e3883d50a4075a095fd6df3505910540a4
R. Miles McCain <github@sendmiles.email>
2022-11-25 17:34:54 -0500

    Make home page sneaky

--- platform/test/platform_web/controllers/user_session_controller_test.exs
+++ platform/test/platform_web/controllers/user_session_controller_test.exs
@@ -31,10 +31,10 @@ defmodule PlatformWeb.UserSessionControllerTest do
       assert get_session(conn, :user_token)
       assert redirected_to(conn) == "/"
 
-      # Now do a logged in request and verify they are redirected to /map
+      # Now do a logged in request and verify they are redirected to /incidents
       conn = get(conn, "/")
       response = html_response(conn, 302)
-      assert response =~ "/home"
+      assert response =~ "/incidents"
     end
 
     test "logs the user in with return to", %{conn: conn, user: user} do
