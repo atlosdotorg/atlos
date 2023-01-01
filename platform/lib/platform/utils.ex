47c1321b0179a2ee77d461d9273eeae26e5f7de5
R. Miles McCain <github@sendmiles.email>
2023-01-01 13:27:15 -0500

    Make slugs 7 characters, no ATL

--- platform/lib/platform/utils.ex
+++ platform/lib/platform/utils.ex
@@ -10,7 +10,7 @@ defmodule Platform.Utils do
   def get_tag_regex(), do: @tag_regex
 
   def generate_media_slug() do
-    slug = for _ <- 1..12, into: "", do: <<Enum.random('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')>>
+    slug = for _ <- 1..7, into: "", do: <<Enum.random('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')>>
 
     if !is_nil(Platform.Material.get_full_media_by_slug(slug)) do
       generate_media_slug()
