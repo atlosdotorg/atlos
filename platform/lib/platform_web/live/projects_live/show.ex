0b8ffb06d90624a62a3ce3be3a178ca7145bda4b
R. Miles McCain <github@sendmiles.email>
2023-01-07 21:55:02 -0500

    Mobile improvements

--- platform/lib/platform_web/live/projects_live/show.ex
+++ platform/lib/platform_web/live/projects_live/show.ex
@@ -84,7 +84,7 @@ defmodule PlatformWeb.ProjectsLive.Show do
               <% end %>
             </div>
           </div>
-          <nav class="flex space-x-8 mt-8" aria-label="Tabs">
+          <nav class="flex space-x-8 mt-8 overflow-x-auto" aria-label="Tabs">
             <% inactive_classes =
               "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 group inline-flex items-center py-4 px-1 border-b-2 font-medium text-sm" %>
             <% active_classes =
@@ -134,7 +134,7 @@ defmodule PlatformWeb.ProjectsLive.Show do
               class={inactive_classes}
             >
               <Heroicons.magnifying_glass mini class="opacity-75 -ml-0.5 mr-2 h-5 w-5" />
-              <span>Search &rarr;</span>
+              <span>Search&nbsp;&rarr;</span>
             </.link>
           </nav>
         </article>
