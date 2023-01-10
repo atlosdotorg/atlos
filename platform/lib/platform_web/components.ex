e8839dd1c57ffb6cf814fa4344cb5a89796927f6
R. Miles McCain <github@sendmiles.email>
2023-01-10 14:30:42 -0800

    Add copy URL to media display

--- platform/lib/platform_web/components.ex
+++ platform/lib/platform_web/components.ex
@@ -2674,6 +2674,20 @@ defmodule PlatformWeb.Components do
                       Copy Hash Info
                     </button>
                   <% end %>
+                  <button
+                    type="button"
+                    rel="nofollow"
+                    role="menuitem"
+                    title="Copy Hash Information"
+                    class="text-gray-700 px-2 py-2 text-sm flex items-center gap-2 hover:bg-gray-100 w-full"
+                    onclick={
+                      "window.setClipboard(" <>
+                        Jason.encode!(@version.source_url) <>
+                        ")"
+                    }
+                  >
+                    <Heroicons.link mini class="w-5 h-5 text-neutral-500" /> Copy URL
+                  </button>
                   <%= if @version.visibility == :visible and @show_controls do %>
                     <button
                       type="button"
