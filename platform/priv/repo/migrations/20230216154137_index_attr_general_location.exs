00ff58633652ce467c43cac514f0fc37774209b3
R. Miles McCain <github@sendmiles.email>
2023-02-16 07:42:53 -0800

    Fix general location not being indexed

---
+++ platform/priv/repo/migrations/20230216154137_index_attr_general_location.exs
@@ -0,0 +1,58 @@
+defmodule Platform.Repo.Migrations.IndexAttrGeneralLocation do
+  use Ecto.Migration
+
+  def change do
+    execute "ALTER TABLE media DROP COLUMN searchable",
+            """
+            ALTER TABLE media
+            ADD COLUMN searchable tsvector
+            GENERATED ALWAYS AS (
+              to_tsvector('english',
+                imm_array_to_string(
+                  coalesce(slug, '') || ARRAY[]::text[] ||
+
+                  coalesce(attr_description, '') || ARRAY[]::text[] ||
+                  coalesce(attr_more_info, '') || ARRAY[]::text[] ||
+                  coalesce(attr_type, '{}') || ARRAY[]::text[] ||
+                  coalesce(attr_impact, '{}') || ARRAY[]::text[] ||
+                  coalesce(attr_equipment, '{}') || ARRAY[]::text[] ||
+
+                  coalesce(attr_restrictions, '{}') || ARRAY[]::text[] ||
+                  coalesce(attr_sensitive, '{}') || ARRAY[]::text[] ||
+                  coalesce(attr_status, '') || ARRAY[]::text[] ||
+                  coalesce(attr_tags, '{}'),
+                  ' ', ''
+                )
+              ) || to_tsvector('english', auto_metadata)
+            ) STORED
+            """
+            |> String.replace("\n", " ")
+
+    execute """
+            ALTER TABLE media
+            ADD COLUMN searchable tsvector
+            GENERATED ALWAYS AS (
+              to_tsvector('english',
+                imm_array_to_string(
+                  coalesce(slug, '') || ARRAY[]::text[] ||
+
+                  coalesce(attr_description, '') || ARRAY[]::text[] ||
+                  coalesce(attr_more_info, '') || ARRAY[]::text[] ||
+                  coalesce(attr_general_location, '') || ARRAY[]::text[] ||
+                  coalesce(attr_type, '{}') || ARRAY[]::text[] ||
+                  coalesce(attr_impact, '{}') || ARRAY[]::text[] ||
+                  coalesce(attr_equipment, '{}') || ARRAY[]::text[] ||
+
+                  coalesce(attr_restrictions, '{}') || ARRAY[]::text[] ||
+                  coalesce(attr_sensitive, '{}') || ARRAY[]::text[] ||
+                  coalesce(attr_status, '') || ARRAY[]::text[] ||
+                  coalesce(attr_tags, '{}'),
+                  ' ', ''
+                )
+              ) || to_tsvector('english', auto_metadata)
+            ) STORED
+            """
+            |> String.replace("\n", " "),
+            "ALTER TABLE media DROP COLUMN searchable"
+  end
+end
