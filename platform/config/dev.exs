cf72ea229ba38cb249a286912bf9fe0567f0f7d4
R. Miles McCain <github@sendmiles.email>
2023-02-28 10:07:12 -0800

    Move local back to dev

--- platform/config/dev.exs
+++ platform/config/dev.exs
@@ -5,7 +5,7 @@ config :platform, Platform.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "platform_bellingcat",
+  database: "platform_dev",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10,
   types: Platform.Repo.PostgresTypes
