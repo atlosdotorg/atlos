5e97f84db5ec30db8f9411542832691a3b4375a1
R. Miles McCain <github@sendmiles.email>
2023-06-17 20:05:34 -0700

    Add ssl as extra application

--- platform/mix.exs
+++ platform/mix.exs
@@ -20,7 +20,7 @@ defmodule Platform.MixProject do
   def application do
     [
       mod: {Platform.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
