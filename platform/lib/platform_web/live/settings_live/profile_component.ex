097995ad9f09c47deb57105511bbad1dbfcbe8ff
Jett Chen <jettchen12345@gmail.com>
2023-10-10 13:32:25 +0000

    fix profile photo update

--- platform/lib/platform_web/live/settings_live/profile_component.ex
+++ platform/lib/platform_web/live/settings_live/profile_component.ex
@@ -127,7 +127,7 @@ defmodule PlatformWeb.SettingsLive.ProfileComponent do
                     >
                       Change
                     </button>
-                    <.live_file_input upload={@uploads.bulk_upload} class="sr-only" />
+                    <.live_file_input upload={@uploads.profile_photo_file} class="sr-only" />
                   </label>
                   <%= hidden_input(f, :profile_photo_file) %>
                 </div>
