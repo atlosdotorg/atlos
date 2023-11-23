be928bc0dfe2f12a26da74aa67858be8778f8540
Jett Chen <jettchen12345@gmail.com>
2023-11-23 13:41:06 +0000

    add initial zip export support

--- platform/lib/platform_web/controllers/media/export_controller.ex
+++ platform/lib/platform_web/controllers/media/export_controller.ex
@@ -1,9 +1,12 @@
 defmodule PlatformWeb.ExportController do
+  require Logger
   use PlatformWeb, :controller
 
   alias Platform.Material
   alias Material.Attribute
   alias Material.MediaSearch
+  alias Material.Media
+  alias PlatformWeb.HTTPDownload
 
   defp format_media(%Material.Media{} = media, fields) do
     {lon, lat} =
@@ -74,56 +77,101 @@ defmodule PlatformWeb.ExportController do
     |> to_string()
   end
 
+  # def create(conn, params) do
+  #   c = MediaSearch.changeset(params)
+  #   {full_query, _} = MediaSearch.search_query(c)
+  #   final_query = MediaSearch.filter_viewable(full_query, conn.assigns.current_user)
+  #   results = Material.query_media(final_query, for_user: conn.assigns.current_user)
+
+  #   max_num_versions =
+  #     Enum.max(
+  #       results
+  #       |> Enum.map(fn media ->
+  #         length(media.versions |> Enum.filter(&(&1.visibility == :visible)))
+  #       end),
+  #       fn -> 0 end
+  #     )
+
+  #   Temp.track!()
+  #   path = Temp.path!(suffix: "atlos-export.csv")
+  #   file = File.open!(path, [:write, :utf8])
+
+  #   fields_excluding_custom =
+  #     [:slug, :project, :inserted_at, :updated_at, :latitude, :longitude] ++
+  #       Attribute.attribute_names() ++
+  #       Enum.map(1..max_num_versions, &("source_" <> to_string(&1)))
+
+  #   # Remove "weird" fields that are redundant or not useful
+  #   fields_excluding_custom =
+  #     Enum.reject(fields_excluding_custom, fn field ->
+  #       field in [:geolocation]
+  #     end)
+
+  #   formatted = Enum.map(results, &format_media(&1, fields_excluding_custom))
+  #   media = formatted |> Enum.map(fn {media, _} -> media end)
+
+  #   custom_attribute_names =
+  #     formatted |> Enum.map(fn {_, fields} -> fields end) |> List.flatten() |> Enum.uniq()
+
+  #   media
+  #   |> CSV.encode(
+  #     headers:
+  #       (fields_excluding_custom ++ custom_attribute_names) |> Enum.map(&format_field_name/1),
+  #     escape_formulas: true
+  #   )
+  #   |> Enum.each(&IO.write(file, &1))
+
+  #   :ok = File.close(file)
+
+  #   Platform.Auditor.log(:bulk_export, params, conn)
+
+  #   # The filename should be atlos-export-YYYY-MM-DD.csv
+  #   user_visible_filename = "atlos-export-#{Date.utc_today()}.csv"
+  #   send_download(conn, {:file, path}, filename: user_visible_filename)
+  # end
+
   def create(conn, params) do
     c = MediaSearch.changeset(params)
+    root_folder_name = "atlos-export-#{Date.utc_today()}"
     {full_query, _} = MediaSearch.search_query(c)
     final_query = MediaSearch.filter_viewable(full_query, conn.assigns.current_user)
-    results = Material.query_media(final_query, for_user: conn.assigns.current_user)
+    results =
+      Material.query_media(final_query, for_user: conn.assigns.current_user)
+      |> List.flatten()
+      |> Enum.map(fn media ->
+        media_slug = Media.slug_to_display(media)
+        Logger.debug("Checking media #{media_slug}")
+        media.versions
+        |> Enum.map( fn version ->
+          Logger.debug("Checking version #{media_slug}/#{version.scoped_id}")
+          folder_name = "#{root_folder_name}/#{media_slug}/#{media_slug}-#{version.scoped_id}"
+          version.artifacts
+          |> Enum.map(fn artifact ->
+            location = Material.media_version_artifact_location(artifact)
+            f_extension = artifact.file_location |> String.split(".") |> List.last()
+            fname = "#{artifact.type}_#{media_slug}-#{version.scoped_id}.#{f_extension}"
+            Logger.debug("Artifact #{fname}: #{location}")
+            Zstream.entry("#{folder_name}/#{fname}", HTTPDownload.stream!(location))
+          end)
+        end)
+        end)
+      |> List.flatten()
+      |> List.flatten()
+      |> Zstream.zip()
 
-    max_num_versions =
-      Enum.max(
-        results
-        |> Enum.map(fn media ->
-          length(media.versions |> Enum.filter(&(&1.visibility == :visible)))
-        end),
-        fn -> 0 end
-      )
+    Logger.debug("Sending file: #{inspect(results)}")
 
-    Temp.track!()
-    path = Temp.path!(suffix: "atlos-export.csv")
-    file = File.open!(path, [:write, :utf8])
+    conn = conn
+    |> put_resp_content_type("application/zip")
+    |> put_resp_header("content-disposition", "attachment; filename=\"#{root_folder_name}.zip\"")
+    |> send_chunked(:ok)
 
-    fields_excluding_custom =
-      [:slug, :project, :inserted_at, :updated_at, :latitude, :longitude] ++
-        Attribute.attribute_names() ++
-        Enum.map(1..max_num_versions, &("source_" <> to_string(&1)))
+    results
+    |> Stream.chunk_every(128) # Not sure what chunk size to choose
+    |> Enum.map(fn chn ->
+      conn |> chunk(chn)
+    end)
 
-    # Remove "weird" fields that are redundant or not useful
-    fields_excluding_custom =
-      Enum.reject(fields_excluding_custom, fn field ->
-        field in [:geolocation]
-      end)
-
-    formatted = Enum.map(results, &format_media(&1, fields_excluding_custom))
-    media = formatted |> Enum.map(fn {media, _} -> media end)
-
-    custom_attribute_names =
-      formatted |> Enum.map(fn {_, fields} -> fields end) |> List.flatten() |> Enum.uniq()
-
-    media
-    |> CSV.encode(
-      headers:
-        (fields_excluding_custom ++ custom_attribute_names) |> Enum.map(&format_field_name/1),
-      escape_formulas: true
-    )
-    |> Enum.each(&IO.write(file, &1))
-
-    :ok = File.close(file)
-
-    Platform.Auditor.log(:bulk_export, params, conn)
-
-    # The filename should be atlos-export-YYYY-MM-DD.csv
-    user_visible_filename = "atlos-export-#{Date.utc_today()}.csv"
-    send_download(conn, {:file, path}, filename: user_visible_filename)
+    conn
   end
 end
---
+++ platform/lib/platform_web/controllers/media/http_download.ex
@@ -0,0 +1,49 @@
+defmodule PlatformWeb.HTTPDownload do
+
+  def stream!(url) do
+    Stream.resource(
+      fn -> start_request(url) end,
+      fn ref ->
+        case receive_response(ref) do
+          #returning the chunk to the stream
+          {:ok, {:chunk, chunk}} ->
+            HTTPoison.stream_next(ref)
+            {[chunk], ref}
+          {:ok, msg} ->
+            IO.inspect(msg)
+            HTTPoison.stream_next(ref)
+            {[], ref}
+          {:error, error} ->
+            IO.puts("ERROR")
+            raise("error #{inspect error}")
+          :done -> {:halt, ref}
+        end
+      end,
+      fn ref -> :hackney.stop_async(ref) end
+    )
+  end
+
+
+  defp start_request(url) do
+    {:ok, ref} = HTTPoison.get(url, %{}, stream_to: self(), async: :once)
+    ref
+  end
+
+  defp receive_response(ref) do
+    id = ref.id
+    receive do
+      %HTTPoison.AsyncStatus{code: code, id: ^id} when 200 <= code and code < 300 ->
+        {:ok, {:status_code, code}}
+      %HTTPoison.AsyncStatus{code: code, id: ^id} ->
+        {:error, {:status_code, code}}
+
+      %HTTPoison.AsyncHeaders{headers: headers, id: ^id}->
+        {:ok, {:headers, headers}}
+
+      %HTTPoison.AsyncChunk{chunk: chunk, id: ^id}->
+        {:ok, {:chunk, chunk}}
+
+      %HTTPoison.AsyncEnd{id: ^id}-> :done
+    end
+  end
+end
