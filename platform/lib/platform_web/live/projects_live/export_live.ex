757aee07d6f2484ba0cf7cd72f572c80a1f43176
Jett Chen <jettchen12345@gmail.com>
2023-11-29 03:40:28 +0000

    switch to card

--- platform/lib/platform_web/live/projects_live/export_live.ex
+++ platform/lib/platform_web/live/projects_live/export_live.ex
@@ -5,13 +5,13 @@ defmodule PlatformWeb.ProjectsLive.ExportComponent do
 
   def render(assigns) do
     ~H"""
-    <section class="flex flex-col md:flex-row gap-4 py-8">
+    <section class="flex flex-col md:flex-row my-8">
       <div class="mb-4 md:w-[20rem] md:min-w-[20rem] md:mr-20">
         <p class="sec-head text-xl">Export</p>
         <p class="sec-subhead">Export Project Metadata or Media</p>
       </div>
-      <div class="grow">
-        <div class="mt-4 mb-2">
+      <.card class="grow border">
+        <div class="mb-2">
           <p class="sec-head">Export Incidents</p>
           <p class="sec-subhead mb-2">Export metadata about all incidents in this project.</p>
           <%= button type: "button", to: Routes.export_path(@socket, :create_csv_export, %{"project_id" => @project.id}),
@@ -24,24 +24,22 @@ defmodule PlatformWeb.ProjectsLive.ExportComponent do
           <% end %>
         </div>
 
-        <%= if Permissions.can_export_full?(@current_user, @project) do %>
-          <div class="mt-4 mb-2">
-            <p class="sec-head">Full Export</p>
-            <p class="sec-subhead mb-2">
-              Export all metadata and media in this project.
-              Note that this is an expensive operation and may take a long time to complete.
-            </p>
-            <%= button type: "button", to: Routes.export_path(@socket, :create_project_full_export, %{"project_id" => @project.id}),
-              class: "base-button",
-              role: "menuitem",
-              method: :post
-            do %>
-              <Heroicons.folder_arrow_down mini class="-ml-0.5 mr-2 h-5 w-5 text-neutral-400" />
-              All Data & Media (ZIP)
-            <% end %>
-          </div>
-        <% end %>
-      </div>
+        <div class="mt-8 mb-2">
+          <p class="sec-head">Full Export</p>
+          <p class="sec-subhead mb-2">
+            Export all metadata and media in this project.
+            Note that this is an expensive operation and may take a long time to complete.
+          </p>
+          <%= button type: "button", to: Routes.export_path(@socket, :create_project_full_export, %{"project_id" => @project.id}),
+            class: "base-button",
+            role: "menuitem",
+            method: :post
+          do %>
+            <Heroicons.folder_arrow_down mini class="-ml-0.5 mr-2 h-5 w-5 text-neutral-400" />
+            All Data & Media (ZIP)
+          <% end %>
+        </div>
+      </.card>
     </section>
     """
   end
