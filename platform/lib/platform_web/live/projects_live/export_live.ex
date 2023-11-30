447d744fdfc686cffc83241ae274b962a0e0416a
Jett Chen <jettchen12345@gmail.com>
2023-11-30 02:49:46 +0000

    move info box to top of card

--- platform/lib/platform_web/live/projects_live/export_live.ex
+++ platform/lib/platform_web/live/projects_live/export_live.ex
@@ -10,36 +10,64 @@ defmodule PlatformWeb.ProjectsLive.ExportComponent do
         <p class="sec-head text-xl">Export</p>
         <p class="sec-subhead">Export Project Metadata or Media</p>
       </div>
-      <.card class="grow border">
-        <div class="mb-2">
-          <p class="sec-head">Export Incidents</p>
-          <p class="sec-subhead mb-2">Export metadata about all incidents in this project.</p>
-          <%= button type: "button", to: Routes.export_path(@socket, :create_csv_export, %{"project_id" => @project.id}),
-            class: "base-button",
-            role: "menuitem",
-            method: :post
-          do %>
-            <Heroicons.document_arrow_down mini class="-ml-0.5 mr-2 h-5 w-5 text-neutral-400" />
-            Incidents (CSV)
-          <% end %>
+      <div class="grow">
+        <div class="rounded-md bg-blue-50 p-4 border-blue-600 border mb-8">
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <Heroicons.information_circle mini class="h-5 w-5 text-blue-500" />
+            </div>
+            <div class="ml-3 flex-1 md:flex flex-col text-sm text-blue-700 md:justify-between prose prose-sm">
+              <p class="text-sm text-blue-700">
+                A full export may take a long time to complete.
+              </p>
+              <details class="-mt-2">
+                <summary class="text-sm text-blue-700 cursor-pointer">
+                  Learn more about the structure of the ZIP file
+                </summary>
+                <div class="mt-2 text-sm text-blue-700">
+                  <p>
+                    This operation will create a ZIP file containing all media and metadata in this project.
+                    This may take a long time to complete.
+                  </p>
+                  <p class="mt-2">
+                    The ZIP file will be available for download from the Exports page.
+                  </p>
+                </div>
+              </details>
+              </div>
+          </div>
         </div>
+        <.card class="grow border">
+          <div class="mb-2">
+            <p class="sec-head">Export Incidents</p>
+            <p class="sec-subhead mb-2">Export metadata about all incidents in this project.</p>
+            <%= button type: "button", to: Routes.export_path(@socket, :create_csv_export, %{"project_id" => @project.id}),
+              class: "base-button",
+              role: "menuitem",
+              method: :post
+            do %>
+              <Heroicons.document_arrow_down mini class="-ml-0.5 mr-2 h-5 w-5 text-neutral-400" />
+              Incidents (CSV)
+            <% end %>
+          </div>
 
-        <div class="mt-8 mb-2">
-          <p class="sec-head">Full Export</p>
-          <p class="sec-subhead mb-2">
-            Export all metadata and media in this project.
-            Note that this is an expensive operation and may take a long time to complete.
-          </p>
-          <%= button type: "button", to: Routes.export_path(@socket, :create_project_full_export, %{"project_id" => @project.id}),
-            class: "base-button",
-            role: "menuitem",
-            method: :post
-          do %>
-            <Heroicons.folder_arrow_down mini class="-ml-0.5 mr-2 h-5 w-5 text-neutral-400" />
-            All Data & Media (ZIP)
-          <% end %>
-        </div>
-      </.card>
+          <div class="mt-8 mb-2">
+            <p class="sec-head">Full Export</p>
+            <p class="sec-subhead mb-2">
+              Export all metadata and media in this project.
+              Note that this is an expensive operation and may take a long time to complete.
+            </p>
+            <%= button type: "button", to: Routes.export_path(@socket, :create_project_full_export, %{"project_id" => @project.id}),
+              class: "base-button",
+              role: "menuitem",
+              method: :post
+            do %>
+              <Heroicons.folder_arrow_down mini class="-ml-0.5 mr-2 h-5 w-5 text-neutral-400" />
+              All Data & Media (ZIP)
+            <% end %>
+          </div>
+        </.card>
+      </div>
     </section>
     """
   end
