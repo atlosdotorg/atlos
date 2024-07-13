2f6e3fe680647717dd8c1f789017e6c5358671d4
R. Miles McCain <github@sendmiles.email>
2024-07-13 16:14:08 +0000

    Make attribute values searchable (closes #1001)

---
+++ platform/priv/repo/migrations/20240713161134_make_attributes_searchable.exs
@@ -0,0 +1,29 @@
+defmodule Platform.Repo.Migrations.MakeAttributesSearchable do
+  use Ecto.Migration
+
+  def change do
+    # Drop the 'searchable' column from the 'media' table
+    execute "ALTER TABLE media DROP COLUMN searchable;"
+
+    # Add the 'searchable' column to the 'media' table
+    execute """
+              ALTER TABLE media
+              ADD COLUMN searchable tsvector
+              GENERATED ALWAYS AS (
+                setweight(to_tsvector('simple', coalesce(slug, '')), 'A') ||
+                setweight(to_tsvector('simple', coalesce(attr_description, '')), 'A') ||
+                setweight(to_tsvector('simple', coalesce(attr_more_info, '')), 'B') ||
+                setweight(jsonb_to_tsvector('simple', coalesce(project_attributes, '{}'), '"string"'), 'B') ||
+                setweight(to_tsvector('simple', coalesce(attr_general_location, '')), 'C') ||
+                setweight(to_tsvector('simple', imm_array_to_string(attr_restrictions, ' ', ' ')), 'C') ||
+                setweight(to_tsvector('simple', imm_array_to_string(attr_sensitive, ' ', ' ')), 'C') ||
+                setweight(to_tsvector('simple', coalesce(attr_status, '')), 'C') ||
+                setweight(jsonb_to_tsvector('simple', coalesce(auto_metadata, '{}'), '"string"'), 'D')
+              ) STORED
+            """,
+            ""
+
+    # Add an index to the 'media' table on searchable
+    create index(:media, ["searchable"], using: "GIN")
+  end
+end
